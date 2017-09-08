0ca32bc1ead8e815a4e1a10601cc7aa890cd2068
Ren Chunhui <346878847@qq.com>
2017-09-08 04:58:45 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,12 @@
+#! Homebrew 安装
+echo "Homebrew 安装"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#! Tmux 安装
+brew install tmux
+#! Vim
+brew install vim --with-override-systme-vim
+
+#! 软链接
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
